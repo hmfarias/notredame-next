31a64f51112de8f12faf182c50cfbe7265938c35
Marcelo Farias <hmfarias7@gmail.com>
2025-01-22 14:42:16 -0300

    Filter link highlighted when selected

--- app/products/layout.js
+++ app/products/layout.js
@@ -1,40 +1,13 @@
-import PageTitle from '@/components/PageTitle';
-import Link from 'next/link';
+import ProductLayoutClient from '@/components/ProductLayoutClient';
 
 const ProductLayout = async ({ children }) => {
-	// Here I fetch the categories from the api
+	// Fetch categories from the API
 	const endpointBase = 'https://dummyjson.com/products';
-
 	const endpoint = `${endpointBase}/categories`;
 	const data = await fetch(endpoint);
 	const categories = await data.json();
 
-	console.log(categories);
-
-	return (
-		<div className="flex flex-col md:flex-row gap-8">
-			<aside className="min-w-[200px]">
-				<PageTitle>Filters</PageTitle>
-				<div className="flex flex-col ml-2 md:ml-4">
-					<Link href="/products">All Products</Link>
-					{/* There are too many categories, I only bring 7 */}
-					{categories.slice(0, 7).map((category) => {
-						return (
-							<Link key={category.slug} href={`/products/${category.slug}`}>
-								{category.name}
-							</Link>
-						);
-					})}
-				</div>
-			</aside>
-			<div className="grow">{children}</div>
-		</div>
-	);
+	return <ProductLayoutClient categories={categories}>{children} </ProductLayoutClient>;
 };
 
 export default ProductLayout;
-
-// Para usar luego como class en el link de las categorías
-// className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3${
-// 									pathName === link.href ? 'bg-sky-100 text-blue-600' : ''
-// 								}`}
---
+++ components/ProductLayoutClient.js
@@ -0,0 +1,46 @@
+// This is the layout for the products page. It fetches the categories from the api and displays them in a list.
+'use client';
+import { usePathname } from 'next/navigation';
+import PageTitle from '@/components/PageTitle';
+import Link from 'next/link';
+
+const ProductLayoutClient = ({ categories, children }) => {
+	const pathname = usePathname(); // Get the current path
+	console.log(categories);
+
+	return (
+		<div className="flex flex-col md:flex-row gap-8">
+			<aside className="min-w-[200px]">
+				<PageTitle>Filters</PageTitle>
+				<div className="flex flex-col ml-2 md:ml-4">
+					<Link
+						href="/products"
+						className={`hover:text-gray-500 ${
+							pathname === '/products' ? 'font-bold text-accent bg-accent/30' : ''
+						}`}
+					>
+						All Products
+					</Link>
+					{/* There are too many categories, I only bring 7 */}
+					{categories.slice(0, 7).map((category) => {
+						const categoryPath = `/products/${category.slug}`;
+						return (
+							<Link
+								key={category.slug}
+								href={categoryPath}
+								className={`hover:text-gray-500 ${
+									pathname === categoryPath ? 'font-bold text-accent bg-accent/30' : ''
+								}`}
+							>
+								{category.name}
+							</Link>
+						);
+					})}
+				</div>
+			</aside>
+			<div className="grow">{children}</div>
+		</div>
+	);
+};
+
+export default ProductLayoutClient;
